52ab910ec21e6cb675b9e98c2cbbdc8e6ca1dae5
Chris Widhelm <cwidhelm@contego.net>
2018-01-04 07:57:13 -0600

    Update choco.bat

--- choco.bat
+++ choco.bat
@@ -1,1 +1,30 @@
+# Management
+choco install powershell
+
+# Browsers
+choco install googlechrome
+choco install firefox
+
+# Chat
+choco install slack
+choco install hipchat
+
+# SSH Utilities
 choco install winscp
+choco install putty
+
+# Cloud Storage
+choco install dropbox
+choco install onedrive
+
+# Productivity
+choco install atom
+choco install office365proplus
+
+# Security
+choco install lastpass
+choco install openvpn
+
+# Tracking
+choco install toggl
+choco install rescuetime
