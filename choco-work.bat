b9fdf699eeb011b6be1eae999e24692561f921dc
Chris Widhelm <cwidhelm@contego.net>
2018-08-09 07:26:49 -0500

    slight updates

--- choco-work.bat
+++ choco-work.bat
@@ -11,7 +11,6 @@ choco install postman -y
 
 REM Chat
 choco install slack -y
-choco install hipchat -y
 
 REM SSH Utilities
 choco install winscp -y
@@ -19,30 +18,23 @@ choco install putty -y
 
 REM Cloud Storage
 choco install dropbox -y
-choco install onedrive -y
-choco install googledrive -y
-choco install google-drive-file-stream -y
 
 REM Productivity
 choco install atom -y
-choco install office365proplus -y
 choco install adobereader -y
 choco install lightshot -y
 
 REM Security
 choco install lastpass -y
-choco install openvpn -y
 
-REM Tracking
-choco install toggl -y
-choco install rescuetime -y
+REM Time Tracking
 
 REM SQL
 choco install db-visualizer -y
 choco install sql-server-management-studio -y
 
 REM Virtualization
-choco install rvtools -y
+REM choco install rvtools -y
 
 REM Development
-choco install github -y
+REM choco install github -y
